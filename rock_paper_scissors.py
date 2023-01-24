1aa60294988220895f92947a61dc4b117fa7136d
dannyrebel <danyorebel@gmail.com>
2023-01-24 12:00:15 +0200

    added replayability and a restart mechanism

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,42 +1,55 @@
 import random
 import colorama
 
+while True:
+    rock = "Rock"
+    paper = "Paper"
+    scissors = "Scissors"
 
-rock = "Rock"
-paper = "Paper"
-scissors = "Scissors"
+    # Game move logic
 
-# Game move logic
+    player_move = input(colorama.Fore.LIGHTWHITE_EX + "Choose [r]ock, [p]aper, [s]cissors: ")
 
-player_move = input("Choose [r]ock, [p]aper, [s]cissors:")
+    if player_move == "r" or player_move == "rock":
+        player_move = rock
+    elif player_move == "p" or player_move == "paper":
+        player_move = paper
+    elif player_move == "s" or player_move == "scissors":
+        player_move = scissors
+    else:
+        print("Invalid input. Please try again!")
+        continue
 
-if player_move == "r" or player_move == "rock":
-    player_move = rock
-elif player_move == "p" or player_move == "paper":
-    player_move = paper
-elif player_move == "s" or player_move == "scissors":
-    player_move = scissors
-else:
-    raise SystemExit("Invalid input. Please try again!")
+    computer_choice = random.randint(1, 3)
 
-computer_choice = random.randint(1, 3)
+    if computer_choice == 1:
+        computer_choice = rock
+    elif computer_choice == 2:
+        computer_choice = paper
+    else:
+        computer_choice = scissors
 
-if computer_choice == 1:
-    computer_choice = rock
-elif computer_choice == 2:
-    computer_choice = paper
-else:
-    computer_choice = scissors
+    print(colorama.Fore.BLUE + f"Computer chose {computer_choice}.")
 
-print(colorama.Fore.BLUE + f"Computer chose {computer_choice}.")
+    # Comparing moves and deciding winner
 
-# Comparing moves and deciding winner
+    if (player_move == rock and computer_choice == scissors) or (player_move == paper and computer_choice == rock) \
+            or (player_move == scissors and computer_choice == paper):
+        print(colorama.Fore.GREEN + "You win!")
+    elif (player_move == rock and computer_choice == rock) or (player_move == paper and computer_choice == paper) \
+            or (player_move == scissors and computer_choice == scissors):
+        print(colorama.Fore.LIGHTYELLOW_EX + "Draw!")
+    else:
+        print(colorama.Fore.RED + "You lose!")
 
-if (player_move == rock and computer_choice == scissors) or (player_move == paper and computer_choice == rock) \
-        or (player_move == scissors and computer_choice == paper):
-    print(colorama.Fore.GREEN + "You win!")
-elif (player_move == rock and computer_choice == rock) or (player_move == paper and computer_choice == paper) \
-        or (player_move == scissors and computer_choice == scissors):
-    print(colorama.Fore.YELLOW + "Draw!")
-else:
-    print(colorama.Fore.RED + "You lose!")
+    # Restarting the game
+    while True:
+        restart = str(input(colorama.Fore.LIGHTWHITE_EX + "Play again?: (y / n): "))
+        if restart == "y" or restart == "Y":
+            break
+        elif restart == "n" or restart == "N":
+            print("Goodbye!")
+            raise SystemExit
+        else:
+            print(colorama.Fore.RED + "Invalid option, please confirm your choice.")
+            continue
