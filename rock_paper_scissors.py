4ee70a9fcfda83f599d4442e1342af502ee9c1b6
Byanew23 <borislav.yanev23@gmail.com>
2023-01-24 14:13:17 +0200

    Add extended input and improve readability

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,44 +5,43 @@ player_score = 0
 computer_score = 0
 
 while True:
-    rock = "Rock"
-    paper = "Paper"
-    scissors = "Scissors"
+    rock = ["Rock", "r", "rock", 1]
+    paper = ["Paper", "p", "paper", 2]
+    scissors = ["Scissors", "s", "scissors", 3]
 
     # Game move logic
 
     player_move = input(colorama.Fore.LIGHTWHITE_EX + "Choose [r]ock, [p]aper, [s]cissors: ")
 
-    if player_move == "r" or player_move == "rock":
-        player_move = rock
-    elif player_move == "p" or player_move == "paper":
-        player_move = paper
-    elif player_move == "s" or player_move == "scissors":
-        player_move = scissors
+    if player_move in rock:
+        player_move = rock[0]
+    elif player_move in paper:
+        player_move = paper[0]
+    elif player_move in scissors:
+        player_move = scissors[0]
     else:
         print("Invalid input. Please try again!")
         continue
 
     computer_choice = random.randint(1, 3)
 
-    if computer_choice == 1:
-        computer_choice = rock
-    elif computer_choice == 2:
-        computer_choice = paper
+    if computer_choice in rock:
+        computer_choice = rock[0]
+    elif computer_choice in paper:
+        computer_choice = paper[0]
     else:
-        computer_choice = scissors
+        computer_choice = scissors[0]
 
     print(colorama.Fore.BLUE + f"Computer chose {computer_choice}.")
 
     # Comparing moves and deciding winner
 
-    if (player_move == rock and computer_choice == scissors) or (player_move == paper and computer_choice == rock) \
-            or (player_move == scissors and computer_choice == paper):
+    if player_move == computer_choice:
+        print(colorama.Fore.LIGHTYELLOW_EX + "Draw!")
+    elif (player_move == rock[0] and computer_choice == scissors[0]) or (player_move == paper[0] and computer_choice == rock[0]) \
+            or (player_move == scissors[0] and computer_choice == paper[0]):
         print(colorama.Fore.GREEN + "You win!")
         player_score += 1
-    elif (player_move == rock and computer_choice == rock) or (player_move == paper and computer_choice == paper) \
-            or (player_move == scissors and computer_choice == scissors):
-        print(colorama.Fore.LIGHTYELLOW_EX + "Draw!")
     else:
         print(colorama.Fore.RED + "You lose!")
         computer_score += 1
